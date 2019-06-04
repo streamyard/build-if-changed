823a4ccbb735a10f057ed30118824f24f208f32b
Alec Larson <alec.stanford.larson@gmail.com>
2019-06-04 11:41:36 -0400

    feat: add -s flag for log silencing

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 const hasFlag = require('has-flag')
 require('../src/cli')
-  .run({ force: hasFlag('f') })
+  .run({
+    force: hasFlag('-f'),
+    silent: hasFlag('-s'),
+  })
   .catch(console.error)
